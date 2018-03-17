f503e17ea4b023b0e93b93491c0a372a91213d09
Jefferson Cabral <jeffersoncabral_10@hotmail.com>
2018-03-16 22:14:46 -0300

    Criado app de almoxarifado com o cadastro de fornecedor

---
+++ almoxarifado/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Fornecedor(models.Model):
+    razao_social = models.CharField(max_length=255)
+    segmento = models.CharField(max_length=255)
+    logradouro = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=255)
+    email = models.EmailField()
+    nome_atendente = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.razao_social
