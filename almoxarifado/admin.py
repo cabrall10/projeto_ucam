7c639aa68685f499e009076d083f5b07c972e5cf
Jefferson Cabral <jeffersoncabral_10@hotmail.com>
2018-03-22 10:52:40 -0300

    Criado classe produto no app Almoxarifado

--- almoxarifado/admin.py
+++ almoxarifado/admin.py
@@ -5,4 +5,8 @@ from django.contrib import admin
 
 from .models import Fornecedor
 
+from .models import Produto
+
 admin.site.register(Fornecedor)
+
+admin.site.register(Produto)
--- almoxarifado/models.py
+++ almoxarifado/models.py
@@ -14,3 +14,21 @@ class Fornecedor(models.Model):
 
     def __str__(self):
         return self.razao_social
+
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=255)
+    und_medida = models.CharField(max_length=255)
+    custo = models.CharField(max_length=255)
+    validade = models.CharField(max_length=255)
+    qtd_estoque = models.CharField(max_length=255)
+    classe_tipo = (
+        ('estocavel', 'Estocável'),
+        ('n_estocavel', 'Não estocável'),
+        ('cons_geral', 'Consumo geral'),
+        ('manutencao', 'Manutenção'),
+    )
+    classe_produto = models.CharField(max_length=255, choices=classe_tipo)
+    observacao = models.TextField(max_length=400, default='')
+
+    def __str__(self):
+        return self.nome_produto
