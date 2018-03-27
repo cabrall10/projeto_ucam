184f24b67f7642927425c9c410f9bca70b753116
Tiago Samaha <tiagosamaha@gmail.com>
2018-03-27 19:16:07 -0300

    refatorando atributos do produto

--- almoxarifado/models.py
+++ almoxarifado/models.py
@@ -15,20 +15,22 @@ class Fornecedor(models.Model):
     def __str__(self):
         return self.razao_social
 
+
 class Produto(models.Model):
-    nome_produto = models.CharField(max_length=255)
-    und_medida = models.CharField(max_length=255)
-    custo = models.CharField(max_length=255)
-    validade = models.CharField(max_length=255)
-    qtd_estoque = models.CharField(max_length=255)
-    classe_tipo = (
+    CATEGORIA_CHOICES = (
         ('estocavel', 'Estocável'),
         ('n_estocavel', 'Não estocável'),
         ('cons_geral', 'Consumo geral'),
         ('manutencao', 'Manutenção'),
     )
-    classe_produto = models.CharField(max_length=255, choices=classe_tipo)
-    observacao = models.TextField(max_length=400, default='')
+
+    nome = models.CharField(max_length=255)
+    unidade_medida = models.CharField(max_length=255)
+    custo = models.CharField(max_length=255)
+    validade = models.CharField(max_length=255)
+    quantidade = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=255, choices=CATEGORIA_CHOICES)
+    observacao = models.TextField()
 
     def __str__(self):
         return self.nome_produto
