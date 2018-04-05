936b6a7775642ae0b3e0354638f7e50d24efe879
Jefferson Cabral <jeffersoncabral_10@hotmail.com>
2018-04-05 10:46:12 -0300

    Adicionando search fields e filters para todos os modelos

--- almoxarifado/admin.py
+++ almoxarifado/admin.py
@@ -15,8 +15,21 @@ class EntradaAdmin(admin.ModelAdmin):
     search_fields = ['nota_fiscal', 'fornecedor__razao_social']
     list_filter = ('data',)
 
+class FornecedorAdmin(admin.ModelAdmin):
+    search_fields = ['razao_social', 'segmento']
+    list_filter = ('segmento',)
 
-admin.site.register(Fornecedor)
-admin.site.register(UnidadeMedida)
+class MaterialAdmin(admin.ModelAdmin):
+    search_fields= ['nome', 'categoria']
+    list_filter = ('categoria',)
+
+class Unidade_MedidaAdmin(admin.ModelAdmin):
+    search_fields= ['nome', 'abreviacao']
+    list_filter = ('abreviacao',)
+
+
+
+admin.site.register(Fornecedor, FornecedorAdmin)
+admin.site.register(UnidadeMedida, Unidade_MedidaAdmin)
 admin.site.register(Entrada, EntradaAdmin)
-admin.site.register(Material)
+admin.site.register(Material, MaterialAdmin)
