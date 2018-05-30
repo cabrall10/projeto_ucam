43cf64982b61751f2ffa469f1881ec023ef372c0
cabrall10 <jeffersoncabral_10@hotmail.com>
2018-05-30 12:22:44 -0300

    criando action Gerar relatorio (gerar pdf)

--- almoxarifado/admin.py
+++ almoxarifado/admin.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import datetime
 from django.contrib import admin
 
 from .models import *
 
+
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template.loader import render_to_string
+from weasyprint import HTML
+from django_object_actions import DjangoObjectActions
 
 
 class ItemEntradaAdmin(admin.TabularInline):
@@ -29,7 +36,23 @@ class EntradaAdmin(admin.ModelAdmin):
     inlines = (ItemEntradaAdmin,)
     search_fields = ['nota_fiscal', 'fornecedor__razao_social', 'itemEntrada_material__material__nome']
     list_filter = ('data',)
+    actions = ['relatorio_de_entradas',]
 
+    def relatorio_de_entradas(self, request, queryset):
+        html_string = render_to_string('reports/relatorio_entradas.html', {'objects': queryset})
+        obj = datetime.now().timestamp()
+
+        html = HTML(string=html_string)
+        html.write_pdf(target='/tmp/{}.pdf'.format(obj));
+
+        fs = FileSystemStorage('/tmp')
+        with fs.open('{}.pdf'.format(obj)) as pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename="{}.pdf"'.format(obj)
+        return response
+    relatorio_de_entradas.short_description = "Gerar relatório de entradas"
+
+@admin.register(Fornecedor)
 class FornecedorAdmin(admin.ModelAdmin):
     search_fields = ['razao_social', 'segmento']
     list_filter = ('segmento',)
@@ -45,9 +68,31 @@ class UnidadeMedidaAdmin(admin.ModelAdmin):
 class PessoaFisicaAdmin(admin.ModelAdmin):
     search_fields = ['nome', 'cpf']
 
+@admin.register(ItemEntrada)
+class ReportAdmin(DjangoObjectActions, admin.ModelAdmin):
+    search_fields = ['entrada']
+    list_display = ('material','quantidade')
+
+    def generate_pdf(self, request, obj):
+        html_string = render_to_string('reports/pdf_template.html', {'obj': obj})
+
+        html = HTML(string=html_string)
+        html.write_pdf(target='/tmp/{}.pdf'.format(obj));
+
+        fs = FileSystemStorage('/tmp')
+        with fs.open('{}.pdf'.format(obj)) as pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename="{}.pdf"'.format(obj)
+            return response
+
+        return response
+
+    generate_pdf.label = 'Gerar PDF'
+    generate_pdf.short_description = 'Clique para gerar o PDF dessa ordem de serviço'
+
+    change_actions = ('generate_pdf',)
 
 
-admin.site.register(Fornecedor, FornecedorAdmin)
 admin.site.register(UnidadeMedida, UnidadeMedidaAdmin)
 admin.site.register(Entrada, EntradaAdmin)
 admin.site.register(Material, MaterialAdmin)
