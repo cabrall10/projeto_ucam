aac280704f760522bde86cd7141c6d1325f3a21b
cabrall10 <jeffersoncabral_10@hotmail.com>
2018-06-13 09:56:10 -0300

    corrigindo link para relatorios e configurando html

--- almoxarifado/views.py
+++ almoxarifado/views.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def relatorio_entrada(request):
+    return render(request, 'relatorio_entrada.html')
+
+def relatorio_saida(request):
+    return render(request, 'relatorio_saida.html')
+
+def entradas(request):
+    return render(request, 'entradas.html')
